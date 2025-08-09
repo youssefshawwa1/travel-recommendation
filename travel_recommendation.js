bdfc17e36e231237a973d043656be89fdeb58ad4
youssefshawwa1 <youssefshawwa1@gmail.com>
2025-08-09 17:28:33 +0300

    adding pages

---
+++ travel_recommendation.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+  //   document.addEventListener("DOMContentLoaded", onLoad);
+  //   function onLoad() {
+  //     getPage("home.html");
+  //   }
+  //   function getPage(url) {
+  //     fetch(url)
+  //       .then((response) => response.text())
+  //       .then((response) => console.log(response));
+  //   }
+})();
